7aace18effdcd3472fbadae462eb002e152fd461
JoomlaTema <admin@joomlatema.net>
2025-04-01 18:58:27 +0300

    Update jt_copymoduleassignments.php
    <?php
    /**
     * @package     jt_copymoduleassignments Joomla.Plugin
     * @copyright   Copyright (C) http://www.joomlatema.net, Inc.
     * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
     * @author      JoomlaTema.Net
     * @link        http://www.joomlatema.net
     */
    
    defined('_JEXEC') or die;
    
    use Joomla\CMS\Plugin\CMSPlugin;
    use Joomla\CMS\Factory;
    use Joomla\Database\DatabaseInterface;
    
    /**
     * Copy Module Assignments Plugin
     */
    class PlgContentJt_Copymoduleassignments extends CMSPlugin
    {
        public function onContentAfterSave($context, &$table, $isNew)
        {
            $app = Factory::getApplication();
            $db = Factory::getContainer()->get(DatabaseInterface::class); // Get database instance
    
            // Debugging output
            $app->enqueueMessage('Context: ' . $context);
            $app->enqueueMessage('Is New: ' . ($isNew ? 'Yes' : 'No'));
    
            // Return if invalid context
            if ($context !== 'com_menus.item') {
                $app->enqueueMessage('Invalid context, exiting.', 'error');
                return true;
            }
    
            // Only proceed if the item is new
            if ($isNew) {
                // Get the original menu item ID from the submitted data
                $originalMenuId = $app->input->getInt('id', 0);
    
                // Debugging output
                $app->enqueueMessage('New Menu Item ID: ' . $table->id);
                $app->enqueueMessage('Original Menu ID: ' . $originalMenuId);
    
                // Fetch assigned modules for the original menu ID
                $query = $db->getQuery(true)
                    ->select($db->quoteName('moduleid'))
                    ->from($db->quoteName('#__modules_menu'))
                    ->where($db->quoteName('menuid') . ' = ' . (int) $originalMenuId);
    
                $db->setQuery($query);
    
                try {
                    $modules = (array) $db->loadColumn();
                    $app->enqueueMessage('Modules Found: ' . count($modules));
                } catch (\Exception $e) {
                    $app->enqueueMessage('Error fetching modules: ' . $e->getMessage(), 'error');
                    return false;
                }
    
                // Assign all found modules to copied menu item
                if (!empty($modules)) {
                    foreach ($modules as $mid) {
                        $mdl = (object) [
                            'moduleid' => $mid,
                            'menuid'   => $table->id
                        ];
    
                        try {
                            $db->insertObject('#__modules_menu', $mdl);
                            $app->enqueueMessage('Assigned module ID: ' . $mid . ' to new menu item ID: ' . $table->id);
                        } catch (\Exception $e) {
                            $app->enqueueMessage('Error assigning module ID ' . $mid . ': ' . $e->getMessage(), 'error');
                        }
                    }
                } else {
                    $app->enqueueMessage('No modules to assign', 'warning');
                }
            } else {
                $app->enqueueMessage('Item is being edited, not duplicating.', 'warning');
            }
    
            return true;
        }
    }

--- jt_copymoduleassignments.php
+++ jt_copymoduleassignments.php
@@ -1,84 +1,85 @@
 <?php
-/***
+/**
  * @package     jt_copymoduleassignments Joomla.Plugin
- * @copyright   Copyright (C) http://www.joomlatema.net, Inc. All rights reserved.
- * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
- * @author     	JoomlaTema.Net
- * @link 		http://www.joomlatema.net
- ***/
+ * @copyright   Copyright (C) http://www.joomlatema.net, Inc.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * @author      JoomlaTema.Net
+ * @link        http://www.joomlatema.net
+ */
+
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
 
 /**
  * Copy Module Assignments Plugin
  */
-class PlgContentJt_Copymoduleassignments extends JPlugin
+class PlgContentJt_Copymoduleassignments extends CMSPlugin
 {
-    protected $db;
-
-    public function __construct($app, $plugin)
+    public function onContentAfterSave($context, &$table, $isNew)
     {
-        parent::__construct($app, $plugin);
-        $this->db = JFactory::getDbo();
-    }
-
-  public function onContentAfterSave($context, &$table, $isNew)
-{
-    // Debugging output
-    JFactory::getApplication()->enqueueMessage('Context: ' . $context);
-    JFactory::getApplication()->enqueueMessage('Is New: ' . ($isNew ? 'Yes' : 'No'));
-
-    // Return if invalid context
-    if ($context != 'com_menus.item') {
-        JFactory::getApplication()->enqueueMessage('Invalid context, exiting.', 'error');
-        return true;
-    }
-
-    // Only proceed if the item is new
-    if ($isNew) {
-        // Get the original menu item ID from the submitted data (this assumes the ID is part of the submitted form data)
-        $originalMenuId = JFactory::getApplication()->input->getInt('id', 0);
+        $app = Factory::getApplication();
+        $db = Factory::getContainer()->get(DatabaseInterface::class); // Get database instance
 
         // Debugging output
-        JFactory::getApplication()->enqueueMessage('New Menu Item ID: ' . $table->id);
-        JFactory::getApplication()->enqueueMessage('Original Menu ID: ' . $originalMenuId);
+        $app->enqueueMessage('Context: ' . $context);
+        $app->enqueueMessage('Is New: ' . ($isNew ? 'Yes' : 'No'));
 
-        // Proceed with fetching assigned modules for the original menu ID
-        $query1 = $this->db->getQuery(true)
-            ->select($this->db->quoteName('moduleid'))
-            ->from($this->db->quoteName('#__modules_menu'))
-            ->where($this->db->quoteName('menuid') . ' = ' . (int) $originalMenuId);
-        $this->db->setQuery($query1);
-
-        try {
-            $modules = (array) $this->db->loadColumn();
-            JFactory::getApplication()->enqueueMessage('Modules Found: ' . count($modules));
-        } catch (Exception $e) {
-            JFactory::getApplication()->enqueueMessage('Error fetching modules: ' . $e->getMessage(), 'error');
-            return false;
+        // Return if invalid context
+        if ($context !== 'com_menus.item') {
+            $app->enqueueMessage('Invalid context, exiting.', 'error');
+            return true;
         }
 
-        // Assign all found modules to copied menu item
-        if (!empty($modules)) {
-            foreach ($modules as $mid) {
-                $mdl = new stdClass();
-                $mdl->moduleid = $mid;
-                $mdl->menuid = $table->id; // This is the new menu item ID
-                try {
-                    $this->db->insertObject('#__modules_menu', $mdl);
-                    JFactory::getApplication()->enqueueMessage('Assigned module ID: ' . $mid . ' to new menu item ID: ' . $table->id);
-                } catch (Exception $e) {
-                    JFactory::getApplication()->enqueueMessage('Error assigning module ID ' . $mid . ': ' . $e->getMessage(), 'error');
+        // Only proceed if the item is new
+        if ($isNew) {
+            // Get the original menu item ID from the submitted data
+            $originalMenuId = $app->input->getInt('id', 0);
+
+            // Debugging output
+            $app->enqueueMessage('New Menu Item ID: ' . $table->id);
+            $app->enqueueMessage('Original Menu ID: ' . $originalMenuId);
+
+            // Fetch assigned modules for the original menu ID
+            $query = $db->getQuery(true)
+                ->select($db->quoteName('moduleid'))
+                ->from($db->quoteName('#__modules_menu'))
+                ->where($db->quoteName('menuid') . ' = ' . (int) $originalMenuId);
+
+            $db->setQuery($query);
+
+            try {
+                $modules = (array) $db->loadColumn();
+                $app->enqueueMessage('Modules Found: ' . count($modules));
+            } catch (\Exception $e) {
+                $app->enqueueMessage('Error fetching modules: ' . $e->getMessage(), 'error');
+                return false;
+            }
+
+            // Assign all found modules to copied menu item
+            if (!empty($modules)) {
+                foreach ($modules as $mid) {
+                    $mdl = (object) [
+                        'moduleid' => $mid,
+                        'menuid'   => $table->id
+                    ];
+
+                    try {
+                        $db->insertObject('#__modules_menu', $mdl);
+                        $app->enqueueMessage('Assigned module ID: ' . $mid . ' to new menu item ID: ' . $table->id);
+                    } catch (\Exception $e) {
+                        $app->enqueueMessage('Error assigning module ID ' . $mid . ': ' . $e->getMessage(), 'error');
+                    }
                 }
+            } else {
+                $app->enqueueMessage('No modules to assign', 'warning');
             }
         } else {
-            JFactory::getApplication()->enqueueMessage('No modules to assign', 'warning');
+            $app->enqueueMessage('Item is being edited, not duplicating.', 'warning');
         }
 
-        // Continue with any additional logic for exception modules if needed...
-    } else {
-        JFactory::getApplication()->enqueueMessage('Item is being edited, not duplicating.', 'warning');
+        return true;
     }
-
-    return true;
-}
 }
